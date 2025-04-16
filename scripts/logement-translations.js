dc118f9928e1479c78d2b75c5917fa09011ce808
Cyril Samson <cyril.samson@ne.ch>
2025-04-16 14:41:28 +0200

    logement translation

---
+++ scripts/logement-translations.js
@@ -0,0 +1,11 @@
+const logementTranslations = {
+    fr: {
+        title: "Le logement"
+    },
+    en: {
+        title: "Le logement en anglais !!!"
+    },
+    de: {
+        title: "O'Fil du Doubs"
+    }
+};
