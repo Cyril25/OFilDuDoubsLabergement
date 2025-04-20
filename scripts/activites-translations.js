19c8488a79c2568cf14cab74f9f280320424f6e8
Cyril Samson <cyril.samson@free.fr>
2025-04-20 11:58:19 +0200

    sous titre

--- scripts/activites-translations.js
+++ scripts/activites-translations.js
@@ -1,11 +1,17 @@
 const dataTranslations = {
     fr: {
-        title: "Activités",
+        title: "Activités proche",
+        subtitle: "Découvrez les lieux incontournables et les activités à proximité (Entre 2 et 20 minutes du logement maximum)",
+        description: "Voici une sélection d'activités à proximité du logement. Certaines sont gratuites, d'autres
     },
     en: {
         title: "Activities",
+        subtitle: "Discover the must-see places and activities nearby (Between 2 and 20 minutes from the accommodation maximum)",
+        description: "Here is a selection of activities close to the accommodation. Some are free, others are not.",
     },
     de: {
         title: "Aktivitäten",
+        subtitle: "Entdecken Sie die Sehenswürdigkeiten und Aktivitäten in der Nähe (zwischen 2 und 20 Minuten von der Unterkunft entfernt)",
+        description: "Hier ist eine Auswahl an Aktivitäten in der Nähe der Unterkunft. Einige sind kostenlos, andere nicht.",
     }
 };
