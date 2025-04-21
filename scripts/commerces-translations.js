31232fbee78cd6892ba9da8b1e5fcf4c793070b4
Cyril Samson <cyril.samson@free.fr>
2025-04-21 18:46:43 +0200

    commerce, style

--- scripts/commerces-translations.js
+++ scripts/commerces-translations.js
@@ -2,7 +2,7 @@ const dataTranslations = {
     fr: {
         title: "Commerces du village",
         subtitle: "Tout ce dont vous avez besoin se trouve au coeur de village",
-        introduction: "Le village de Labergement-Sainte-Marie offre une variété de commerces et services à proximité de notre logement. Que vous ayez besoin de faire des courses, de vous restaurer ou de profiter d'activités locales, tout est à portée de main.",
+        introduction: "Le village de Labergement-Sainte-Marie offre une variété de commerces et services à proximité de notre logement, tels qu'une boulangerie, une épicerie, et des restaurants. Que vous ayez besoin de faire des courses ou de vous restaurer, tout est à portée de main.",
     },
     en: {
         // English translations 
