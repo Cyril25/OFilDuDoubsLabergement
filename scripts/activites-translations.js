8092d7477a465ff3c3f64d7814d556a516ac7e25
Cyril Samson <cyril.samson@free.fr>
2025-04-21 07:25:33 +0200

    intro et translastions

--- scripts/activites-translations.js
+++ scripts/activites-translations.js
@@ -2,16 +2,16 @@ const dataTranslations = {
     fr: {
         title: "Activités proche",
         subtitle: "Découvrez les lieux incontournables et les activités à proximité",
-        description: "Entre lacs majestueux, paysages préservés, savoir-faire locaux et aventures en plein air, notre région a tant à offrir. Depuis le logement, partez à la découverte d'une sélection d’activités accessibles en quelques minutes seulement : balades paisibles, sites culturels, visites gourmandes ou expériences insolites. En couple, en famille ou entre amis, chaque sortie promet un moment privilégié, au cœur d’un territoire authentique, généreux et accueillant. (Entre 2 et 20 minutes du logement maximum)",
+        introduction: "Entre lacs majestueux, paysages préservés, savoir-faire locaux et aventures en plein air, notre région a tant à offrir. Depuis le logement, partez à la découverte d'une sélection d’activités accessibles en quelques minutes seulement : balades paisibles, sites culturels, visites gourmandes ou expériences insolites. En couple, en famille ou entre amis, chaque sortie promet un moment privilégié, au cœur d’un territoire authentique, généreux et accueillant. (Entre 2 et 20 minutes du logement maximum)",
     },
     en: {
         title: "Activities",
         subtitle: "Discover the must-see places and activities nearby (Between 2 and 20 minutes from the accommodation maximum)",
-        description: "Here is a selection of activities close to the accommodation. Some are free, others are not.",
+        introduction: "Here is a selection of activities close to the accommodation. Some are free, others are not.",
     },
     de: {
         title: "Aktivitäten",
         subtitle: "Entdecken Sie die Sehenswürdigkeiten und Aktivitäten in der Nähe (zwischen 2 und 20 Minuten von der Unterkunft entfernt)",
-        description: "Hier ist eine Auswahl an Aktivitäten in der Nähe der Unterkunft. Einige sind kostenlos, andere nicht.",
+        introduction: "Hier ist eine Auswahl an Aktivitäten in der Nähe der Unterkunft. Einige sind kostenlos, andere nicht.",
     }
 };
