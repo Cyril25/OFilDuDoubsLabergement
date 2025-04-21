9873d5af6d070b391eff899d62f1d4d4ffc7897d
Cyril Samson <cyril.samson@free.fr>
2025-04-21 13:00:12 +0200

    mise à jour globale

--- scripts/activites-translations.js
+++ scripts/activites-translations.js
@@ -1,8 +1,8 @@
 const dataTranslations = {
     fr: {
-        title: "Activités proche",
+        title: "Activités",
         subtitle: "Découvrez les lieux incontournables et les activités à proximité",
-        introduction: "Entre lacs majestueux, paysages préservés, savoir-faire locaux et aventures en plein air, notre région a tant à offrir. Depuis le logement, partez à la découverte d'une sélection d’activités accessibles en quelques minutes seulement : balades paisibles, sites culturels, visites gourmandes ou expériences insolites. En couple, en famille ou entre amis, chaque sortie promet un moment privilégié, au cœur d’un territoire authentique, généreux et accueillant. (Entre 2 et 20 minutes du logement maximum)",
+        introduction: "Entre lacs majestueux, paysages préservés, savoir-faire locaux et aventures en plein air, notre région a tant à offrir. Depuis le logement, partez à la découverte d'une sélection d’activités accessibles en quelques minutes seulement : balades paisibles, sites culturels, visites gourmandes ou expériences insolites. En couple, en famille ou entre amis, chaque sortie promet un moment privilégié, au cœur d’un territoire authentique, généreux et accueillant. Voici une courte sélection d'activités se trouvant entre 2 et 20 minutes de notre logement maximum.",
         "lac-remoray-title": "Lac de Remoray",
         "lac-remoray-description": "Réserve naturelle protégée, idéale pour les balades, l’observation des oiseaux et des moments de calme au bord de l’eau.",
         "maison-reserve-title": "La Maison de la Réserve",
---
+++ scripts/dispo-translations.js
@@ -0,0 +1,14 @@
+const dataTranslations = {
+    fr: {
+        title: "Disponibilité",
+        subtitle: "Informations pratiques pour votre séjour",
+    },
+    en: {
+        title: "Availability",
+        subtitle: "Practical information for your stay",
+    },
+    de: {
+        title: "Verfügbarkeit",
+        subtitle: "Praktische Informationen für Ihren Aufenthalt",
+    }
+};
