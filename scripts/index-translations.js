1bf85ecf2870c4678113d41e47d080e3a92d076c
Cyril Samson <cyril.samson@free.fr>
2025-04-21 06:56:56 +0200

    traduction accueil et style sous titre

--- scripts/index-translations.js
+++ scripts/index-translations.js
@@ -4,7 +4,10 @@ const dataTranslations = {
         subtitle: "Un hébergement confortable au bord de l'eau",
         subtitle2: "Meublé de tourisme 4 étoiles",
         welcome: "Bienvenue chez Alisson et Cyril - O'Fil du Doubs",
-        description: "Niché à Labergement-Sainte-Marie, notre logement vous accueille dans un cadre paisible et enchanteur, au cœur du Haut-Doubs.<br />Situé à quelques pas des magnifiques lacs de Remoray et de Saint-Point, à proximité immédiate de la frontière suisse et à seulement quelques minutes de la station de ski de Métabief, O'Fil du Doubs est l’endroit idéal pour se ressourcer en toutes saisons.<br /><br />Que vous soyez amateur de randonnée, de sports nautiques, de balades à vélo ou de glisse en hiver, vous trouverez ici un point de départ privilégié pour explorer les merveilles naturelles et culturelles de la région.<br />Nous serons ravis de vous accueillir et de partager avec vous notre coin de paradis !"
+        description1: "Niché à Labergement-Sainte-Marie, notre logement vous accueille dans un cadre paisible et enchanteur, au cœur du Haut-Doubs.",
+        description2: "Situé à quelques pas des magnifiques lacs de Remoray et de Saint-Point, à proximité immédiate de la frontière suisse et à seulement quelques minutes de la station de ski de Métabief, O'Fil du Doubs est l’endroit idéal pour se ressourcer en toutes saisons.",
+        description3: "Que vous soyez amateur de randonnée, de sports nautiques, de balades à vélo ou de glisse en hiver, vous trouverez ici un point de départ privilégié pour explorer les merveilles naturelles et culturelles de la région.",
+        description4: "Nous serons ravis de vous accueillir et de partager avec vous notre coin de paradis !"
     },
     en: {
         title: "O'Fil du Doubs",
